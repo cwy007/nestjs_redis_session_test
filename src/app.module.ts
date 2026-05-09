900b27400a32416d01d5c139127a275bac8480d7
chanweiyan <chanweiyan007@gmail.com>
2026-05-09 19:20:55 +0800

    fix: implement session module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisModule } from './redis/redis.module';
+import { SessionModule } from './session/session.module';
 
 @Module({
-  imports: [RedisModule],
+  imports: [RedisModule, SessionModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/session/session.service.ts
@@ -0,0 +1,28 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { RedisService } from 'src/redis/redis.service';
+
+@Injectable()
+export class SessionService {
+  @Inject(RedisService)
+  private readonly redisService: RedisService;
+
+  async setSession(sessionId: string, data: Record<string, string>, ttl: number = 30 * 60): Promise<string> {
+    if (!sessionId) {
+      sessionId = this.generateSessionId();
+    }
+    await this.redisService.hashSet(`sid_${sessionId}`, data, ttl);
+    return sessionId;
+  }
+
+  async getSession(sessionId: string): Promise<Record<string, string>> {
+    return this.redisService.hashGet(`sid_${sessionId}`);
+  }
+
+  async deleteSession(sessionId: string): Promise<void> {
+    await this.redisService.hashDelete(`sid_${sessionId}`);
+  }
+
+  generateSessionId(): string {
+    return Math.random().toString(36).substring(2) + Date.now().toString(36);
+  }
+}
